9b7a9926b310a8955dd35a9650b3f89bbc96a1a2
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-11-13 05:07:41 +0100

    Updates

---
+++ precache-manifest.c9044e79684041497d1d835b941d0c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8e2f4ea1d5ce1d92d39f88c2603bb2",
+    "url": "/rSensehat-domotica/index.html"
+  },
+  {
+    "revision": "ac6986f4f79cb4215973",
+    "url": "/rSensehat-domotica/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "1a696378000c9a863294",
+    "url": "/rSensehat-domotica/static/css/main.fa6f1d83.chunk.css"
+  },
+  {
+    "revision": "ac6986f4f79cb4215973",
+    "url": "/rSensehat-domotica/static/js/2.d6c476f8.chunk.js"
+  },
+  {
+    "revision": "1a696378000c9a863294",
+    "url": "/rSensehat-domotica/static/js/main.18b4853a.chunk.js"
+  },
+  {
+    "revision": "ad8bc02db2d973569330",
+    "url": "/rSensehat-domotica/static/js/runtime-main.6e759dd3.js"
+  }
+]);
